892d712b89988ee593d560851bb31f581735f29f
azaykarimli <azaykerimli@hotmail.com>
2022-12-12 20:21:16 +0100

    clean up

--- worker.php
+++ worker.php
@@ -1,20 +1,21 @@
 <?php
 
 
-$random_number_array = range(1, 10);
+$random_number_generator_for_array_pop = rand(1, 10);
+$random_number_array = range(2, 10);
 shuffle($random_number_array);
-$random_number_array = array_slice($random_number_array, 0, 5);
-unset($random_number_array[0]);
+$random_number_array_apple = array_slice($random_number_array, 0, $random_number_generator_for_array_pop);
+unset($random_number_array_apple[0]);
 
 
-
-$array = new SplFixedArray(4);
+$array = new SplFixedArray($random_number_generator_for_array_pop);
 $arr = json_decode(json_encode($array), true);
+unset($arr[0]);
 
 //initial array of workers
 //$apple = ['worker_1'=>3,'worker_2'=>1,'worker_3'=>2];
 //$final_apple = ['worker_1'=>0,'worker_2'=>0,'worker_3'=>0];
-$apple = $random_number_array;
+$apple = $random_number_array_apple;
 $final_apple = $arr;
 //print_r($apple);
 
@@ -40,31 +41,14 @@ for ($i = 1; $i <= $random_number_generator; $i++) {
 	$final_apple[$index]++;
 }
 
-echo (worker($final_apple, $apple, $random_number_array));
-
-//echo(final_worker($final_apple, $apple));
-
+echo (worker($final_apple, $apple, $random_number_array_apple));
 
 //function that generates the initial array
 function worker(array $final_apple, array $apple, array $random_number_array): void
 {
 	for ($i = 1; $i <= count($apple); $i++) {
-		/* 
-		echo "Apple worker " . $i . " has " . $apple['worker_' . $i] . " apples to process " . "\r\n";
-		echo "Apple worker " . $i . " gets another " . $final_apple['worker_' . $i] . " apples  (total: " . $apple[$i] . ")" . "\r\n";
- */	
-$finalmente = empty($final_apple[$i]) ? 0: $final_apple[$i];
+		$finalmente = empty($final_apple[$i]) ? 0 : $final_apple[$i]; //if null then 0 of the how many apples does it got
 		echo "Apple worker " . $i . " has " . $random_number_array[$i] . " apples to process " . "\r\n";
 		echo "Apple worker " . $i . " gets another " . $finalmente . " apples  (total: " . $apple[$i] . ")" . "\r\n";
-	}
-}
-//function that generates after the logic
-function final_worker(array $final_apple, array  $apple): void
-{
-	for ($i = 1; $i <= count($final_apple); $i++) {
-
-		echo "Apple worker " . $i . " gets another " . $final_apple['worker_' . $i] . " apples  (total: " . $apple['worker_' . $i] . ")" . "\r\n";
-		//Apple worker 1 gets another 1 apple (total: 4)
-
 	}
 }
