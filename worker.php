f7f9c11a49887fdaf31233c49816020014f83780
azaykarimli <azaykerimli@hotmail.com>
2022-12-13 07:28:21 +0100

    final version of the iteration

--- worker.php
+++ worker.php
@@ -6,12 +6,10 @@ $random_number_generator_for_array_pop = rand(2, 10);
 $random_number_array = range(2, 10);
 shuffle($random_number_array);
 $random_number_array_apple = array_slice($random_number_array, 2, $random_number_generator_for_array_pop);
-unset($random_number_array_apple[0]);
 
 //initial empty array to be filled with distrubution
 $array = new SplFixedArray($random_number_generator_for_array_pop);
 $arr = json_decode(json_encode($array), true);
-unset($arr[0]);
 
 $apple = $random_number_array_apple;
 $apple_counter = $arr;
@@ -40,9 +38,9 @@ echo (worker($apple_counter, $apple, $random_number_array_apple));
 //function that generates the initial array
 function worker(array $apple_counter, array $apple, array $random_number_array): void
 {
-	for ($i = 1; $i <= count($apple); $i++) {
+	for ($i = 0; $i < count($apple); $i++) {
 		$finalmente = empty($apple_counter[$i]) ? 0 : $apple_counter[$i]; //if null then 0 of the how many apples does it got
-		echo "Apple worker " . $i . " has " . $random_number_array[$i] . " apples to process " . "\r\n";
-		echo "Apple worker " . $i . " gets another " . $finalmente . " apples  (total: " . $apple[$i] . ")" . "\r\n";
+		echo "Apple worker " . $i+1 . " has " . $random_number_array[$i] . " apples to process " . "\r\n";
+		echo "Apple worker " . $i+1 . " gets another " . $finalmente . " apples  (total: " . $apple[$i] . ")" . "\r\n";
 	}
 }
