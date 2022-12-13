75a37702548e1476581ffecd76639ed8dd982333
azaykarimli <azaykerimli@hotmail.com>
2022-12-13 10:11:35 +0100

    final version before test

--- worker.php
+++ worker.php
@@ -4,52 +4,75 @@
 //randomly populated array
 //$random_number_generator_for_array_pop = rand(2, 10);
 
-$random_populated_arr = array_map(
-	function () {
-		return rand(2, 10);
-	},
-	array_fill(0, rand(2, 10), null)
-);
+for ($i = 0; $i < 3; $i++) {
 
-$apple = [];
-foreach ($random_populated_arr as $r_p_arr) {
+	$random_populated_arr = array_map(
+		function () {
+			return rand(2, 10);
+		},
+		array_fill(0, rand(2, 10), null)
+	);
 
-	$apple[] = ['worker_after' => $r_p_arr, 'worker' => $r_p_arr, 'counter' => 0];
+
+	foreach ($random_populated_arr as $r_p_arr) {
+		switch ($i) {
+			case 0:
+				$apple[] = ['worker_after' => $r_p_arr, 'worker' => $r_p_arr, 'counter' => 0, "type" => 'Apple'];
+				break;
+			case 1:
+				$orange[] = ['worker_after' => $r_p_arr, 'worker' => $r_p_arr, 'counter' => 0, "type" => 'ORange'];
+				break;
+			case 2:
+				$kiwi[] = ['worker_after' => $r_p_arr, 'worker' => $r_p_arr, 'counter' => 0, "type" => 'Kiwi'];
+				break;
+		}
+	}
 }
 
 sort($apple);
+sort($orange);
+sort($kiwi);
 
 //randomly get number to be distrubuted
 $random_number_generator = rand(1, 10);
 
-echo 'number of apples to be distributed evenly: ' . $random_number_generator . "\r\n";
-
-while ($random_number_generator != 0) {
-	sort($apple); //sort the array each time to get second(min) and min
-	$first = $apple[0]['worker_after'];
-	$second = $apple[1]['worker_after'];
-	$gap = ($second - $first) + 1;
-	if ($random_number_generator <= $gap) {
-
-		$gap = $random_number_generator;
-	}
-	$index = array_search(min($apple), $apple);
-	//echo $index."\r\n";
-	$apple[$index]['worker_after'] += $gap;
-	$apple[$index]['counter'] += $gap;
-
-	$random_number_generator = $random_number_generator - $gap;
+$fruits = [$apple, $orange, $kiwi];
+foreach ($fruits as $fruit) {
+	
+	$final_fruit = distrubuter($fruit, $random_number_generator);
+	echo 'number of apples to be distributed evenly: ' . $random_number_generator . "\r\n";
+	echo (worker($final_fruit));
+	echo "----------" . "\r\n";
 }
 
 
-echo (worker($apple));
-
-//function that generates the initial array
-function worker(array $apple): void
+function worker(array $fruit): void
 {
-	for ($i = 0; $i < count($apple); $i++) {
-		$finalmente = empty($apple[$i]) ? 0 : $apple[$i]['counter']; //if null then 0 of the how many apples does it got
-		echo "Apple worker " . $i + 1 . " had " . $apple[$i]['worker'] . " apples to process " . "\r\n";
-		echo "Apple worker " . $i + 1 . " gets another " . $finalmente . " apples  (total: " . $apple[$i]['worker_after'] . ")" . "\r\n";
+	for ($i = 0; $i < count($fruit); $i++) {
+		$finalmente = empty($fruit[$i]) ? 0 : $fruit[$i]['counter']; //if null then 0 of the how many apples does it got
+		echo $fruit[$i]['type']." worker " . $i + 1 . " had " . $fruit[$i]['worker'] . " apples to process " . "\r\n";
+		echo $fruit[$i]['type']." worker " . $i + 1 . " gets another " . $finalmente . " apples  (total: " . $fruit[$i]['worker_after'] . ")" . "\r\n";
 	}
+}
+
+function distrubuter(array $fruit, $random_number_generator)
+{
+
+	while ($random_number_generator != 0) {
+		sort($fruit); //sort the array each time to get second(min) and min
+		$first = $fruit[0]['worker_after'];
+		$second = $fruit[1]['worker_after'];
+		$gap = ($second - $first) + 1;
+		if ($random_number_generator <= $gap) {
+
+			$gap = $random_number_generator;
+		}
+		$index = array_search(min($fruit), $fruit);
+		//echo $index."\r\n";
+		$fruit[$index]['worker_after'] += $gap;
+		$fruit[$index]['counter'] += $gap;
+
+		$random_number_generator = $random_number_generator - $gap;
+	}
+	return $fruit;
 }
